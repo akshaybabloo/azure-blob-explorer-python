223c8d6edaad8e14f8de47dc92ce5454a1d2b1e6
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-04-28 19:41:43 +1200

    upload_files added

--- azblobexplorer/upload.py
+++ azblobexplorer/upload.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from azure.storage.blob import BlockBlobService
+
 __all__ = ['AzureBlobUpload']
 
 
@@ -5,6 +9,7 @@ class AzureBlobUpload:
     """
     Upload a file or a folder.
     """
+
     def __init__(self, account_name: str, account_key: str, container_name: str):
         """
         :param account_name:
@@ -43,6 +48,30 @@ class AzureBlobUpload:
         else:
             self.block_blob_service.create_blob_from_path(self.container_name,
                                                           upload_to + path.name, path)
+
+    def upload_files(self, files_path: list):
+        """
+        Upload a list of files.
+
+        :param list files_path:
+
+        >>> import os
+        >>> from azblobexplorer import AzureBlobUpload
+        >>> az = AzureBlobUpload('account name', 'account key', 'container name')
+        >>> here = os.path.abspath(os.path.dirname(__file__)) + os.sep
+        >>> path_list = [
+        ...     [os.path.join(here, 'file1.txt'), 'folder_1/'],
+        ...     [os.path.join(here, 'file2.txt'), 'folder_2/'],
+        ...     os.path.join(here, 'file3.txt')
+        ... ]
+        >>> az.upload_files(path_list)
+        """
+
+        for path in files_path:
+            if isinstance(path, list):
+                self.upload_file(path[0], path[1])
+            else:
+                self.upload_file(path)
 
     def upload_folder(self, folder_path: str, upload_to: str):
         """
