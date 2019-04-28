5e3c60d5f88923be611b5eb6b12942093342d23d
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-04-28 15:49:09 +1200

    methods init

--- azblobexplorer/__init__.py
+++ azblobexplorer/__init__.py
@@ -1,1 +1,5 @@
+from .delete import *
+from .download import *
+from .upload import *
+
 __version__ = "1.0"
---
+++ azblobexplorer/upload.py
@@ -0,0 +1,41 @@
+__all__ = ['AzureBlobUpload']
+
+
+class AzureBlobUpload:
+    """
+    Upload a file or a folder.
+    """
+    def __init__(self, account_name: str, account_key: str, container_name: str):
+        """
+        :param account_name:
+            Azure storage account name.
+        :param account_key:
+            Azure storage key.
+        :param container_name:
+            Azure storage container name, URL will be added automatically.
+        """
+        self.account_name = account_name
+        self.account_key = account_key
+        self.container_name = container_name
+
+    def upload_file(self, file_path: str, upload_to: str):
+        """
+        Upload a file to a given blob path.
+
+        :param upload_to:
+            Give the path to upload.
+        :param file_path:
+            Absolute path of the file to upload.
+        """
+        pass
+
+    def upload_folder(self, folder_path: str, upload_to: str):
+        """
+        Upload a folder to a given blob path.
+
+        :param upload_to:
+            Give the path to upload.
+        :param folder_path:
+            Absolute path of the folder to upload.
+        """
+        pass
