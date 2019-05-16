8453748c0ef2ba6c07d413d0efead8ae04d5bb2c
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2019-05-16 16:00:23 +1200

    bump version to 1.1.2

--- _docs/source/conf.py
+++ _docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Akshay Raj Gollahalli'
 author = 'Akshay Raj Gollahalli'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1'
+release = '1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
